e5ddc024a3cf6ba9832b6c3658048b92bd1b142c
nicethanapol <49955977+nicethanapol@users.noreply.github.com>
2020-11-03 17:02:20 +0700

    1

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import {ToolbarModule} from 'primeng/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { Routes ,RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ButtonModule} from 'primeng/button';
+import {CalendarModule} from 'primeng/calendar';
+import {CardModule} from 'primeng/card';
+import { StockComponent } from './stock/stock.component';
+
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'home' , component: HomeComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'register' , component: RegisterComponent},
+  {path: 'stock' , component: StockComponent}
+  
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    StockComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ToolbarModule,
+    ButtonModule,
+    CalendarModule,
+    CardModule,
+    RouterModule.forRoot(appRoutes)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
